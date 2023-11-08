6dcba649eb637c83a4ddc6c742d3379c76444aba
Jonas Kwiedor <mail@gowizzard.de>
2023-11-08 13:38:16 +0100

    fix: Remove static string and replace variable.

--- internal/database/new_client.go
+++ internal/database/new_client.go
@@ -8,7 +8,7 @@ import (
 // NewClient creates a new client for the redis database with user authentication.
 func NewClient(ctx context.Context, o Options) (client *Client, err error) {
 
-	conn, err := (&net.Dialer{}).DialContext(ctx, "tcp", "localhost:6379")
+	conn, err := (&net.Dialer{}).DialContext(ctx, "tcp", o.Address)
 	if err != nil {
 		return nil, err
 	}
